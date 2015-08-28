dc214befd8f3bffa4dcf8f470dc7dfae8a5e00c9
ClaireDavis <cpv26@cornell.edu>
2015-08-28 11:51:52 -0400

    deli line commit

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -16,10 +16,44 @@
     return YES;
 }
 
-/*
- 
- *  Define your methods here!
- 
- */
+// returns string in format: The line is: (list of people)
+// or The line is empty based on the deliLine provided
+- (NSString *)stringWithDeliLine:(NSMutableArray *)deliLine
+{
+  if (!deliLine || deliLine.count == 0) {
+    return @"The line is currently empty.";
+  } else {
+    NSString *deliString = @"The line is:";
+    for (NSUInteger i = 0; i < deliLine.count; i++) {
+      NSUInteger spotInLine = i + 1;
+      NSString *person = deliLine[i];
+      deliString = [NSString stringWithFormat:@"%@\n%lu. %@", deliString, spotInLine, person];
+    }
+    
+    return deliString;
+  }
+}
+
+// adds the name provided to the deliline array
+- (NSMutableArray *)addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine
+{
+  [deliLine addObject:name];
+  return deliLine;
+}
+
+// saves next in line as a string, removes next in line from array,
+// and returns string of item removed
+- (NSString *)serveNextCustomerInDeliLine:(NSMutableArray *)deliLine
+{
+  if (!deliLine || deliLine.count == 0) {
+    return nil;
+  }
+  
+  NSString *nextInLine = [deliLine objectAtIndex:0];
+  [deliLine removeObjectAtIndex:0];
+  
+  return nextInLine;
+}
+
 
 @end
