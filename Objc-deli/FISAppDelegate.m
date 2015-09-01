ca790e0e87c69088d8ef4335f429c411a222be0d
sean-wantwave <sean@wantwave.com>
2015-09-01 15:41:11 -0700

    Added methods for deli line handling

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -16,10 +16,31 @@
     return YES;
 }
 
-/*
- 
- *  Define your methods here!
- 
- */
+- (NSString *)stringWithDeliLine:(NSArray *)deliLine {
+    
+    NSMutableString *deliLineQueue = [[NSMutableString alloc] initWithString:@"The line is:"];
+    
+    if ([deliLine count] == 0) {
+        NSString *lineEmpty = @"The line is currently empty.";
+        return lineEmpty;
+    } else {
+        for (NSUInteger i = 0; i < [deliLine count]; i++) {
+            [deliLineQueue appendString: [NSString stringWithFormat: @"\n%lu. %@", i + 1, deliLine[i]]];
+        }
+        return deliLineQueue;
+    }
+    
+}
+
+- (void)addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine {
+    [deliLine addObject: name];
+    return;
+}
+
+- (NSString *)serveNextCustomerInDeliLine:(NSMutableArray *)deliLine {
+    NSString *firstCustomer = deliLine[0];
+    [deliLine removeObjectAtIndex:0];
+    return firstCustomer;
+}
 
 @end
