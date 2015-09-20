3f20b65f72aec17d8670764ebbfc48043131e224
giwankim <giwan.kim@gmail.com>
2015-09-20 10:29:54 -0400

    OK

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -16,4 +16,30 @@
  
  */
 
+- (NSString *)stringWithDeliLine:(NSArray *)deliLine {
+    NSUInteger lengthOfLine = [deliLine count];
+    
+    if (lengthOfLine == 0) {
+        return @"The line is currently empty.";
+    }
+    
+    NSMutableString *theLine = [@"The line is:" mutableCopy];
+    
+    for (NSUInteger i = 0; i < lengthOfLine; i++) {
+        [theLine appendFormat:@"\n%lu. %@", i + 1, deliLine[i] ];
+    }
+    
+    return theLine;
+}
+
+- (void)addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine {
+    [deliLine addObject:name];
+}
+
+- (NSString *) serveNextCustomerInDeliLine:(NSMutableArray *)deliLine {
+    NSString *firstInLine = deliLine[0];
+    [deliLine removeObjectAtIndex:0];
+    return firstInLine;
+}
+
 @end
