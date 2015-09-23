12f89f5793d30795589483f39b3dc7444961817d
dvxwang <dvxwang@gmail.com>
2015-09-23 16:39:44 -0400

    Lab 11

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -16,4 +16,34 @@
  
  */
 
+-(NSString *) stringWithDeliLine: (NSArray *)deliLine {
+    
+    NSString *result=@"";
+    
+    if ([deliLine count]==0) {
+        result = [result stringByAppendingString:@"The line is currently empty."];
+        return result;
+    }
+    
+    else {
+        result = [result stringByAppendingString:@"The line is:"];
+        for (NSUInteger i=0;i<[deliLine count];i++) {
+            result = [result stringByAppendingFormat:@"\n%li. %@",i+1,deliLine[i]];
+        }
+        return result;
+    }
+    
+}
+
+-(void) addName:(NSString *)name toDeliLine:(NSMutableArray *) deliLine {
+    [deliLine addObject:name];
+}
+
+-(NSString *) serveNextCustomerInDeliLine: (NSMutableArray *) deliLine {
+    
+    NSString *result=deliLine[0];
+    [deliLine removeObjectAtIndex:0];
+    return result;
+}
+
 @end
