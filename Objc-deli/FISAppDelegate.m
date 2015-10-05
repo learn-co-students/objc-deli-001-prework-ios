8aa1d8c573e7a4c33e6b72c6efc04e6307669808
jspringer <jsnsprngr@gmail.com>
2015-10-05 19:48:36 -0400

    Deli Counter

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -16,4 +16,33 @@
  
  */
 
+- (NSString *)stringWithDeliLine:(NSMutableArray *)deliLine {
+    NSString *line = @"The line is:";
+    
+    if ([deliLine count] == 0 ) {
+        return @"The line is currently empty.";
+    } else {
+        for (NSUInteger i = 0; i < [deliLine count]; i++) {
+            NSString *name = [NSString stringWithFormat:@"\n%lu. %@", i+1, deliLine[i]];
+            line = [line stringByAppendingString:name];
+        }
+        
+        return line;
+        
+    }
+    
+}
+
+- (NSMutableArray *)addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine {
+    [deliLine addObject:name];
+    return deliLine;
+}
+
+- (NSString *)serveNextCustomerInDeliLine:(NSMutableArray *)deliLine {
+    NSString *firstCustomer = deliLine[0];
+    [deliLine removeObjectAtIndex:0];
+    
+    return firstCustomer;
+}
+
 @end
