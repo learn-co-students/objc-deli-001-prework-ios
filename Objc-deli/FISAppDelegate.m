3615bcdfc88dba2e821d139fbbbcdc818d9b34cf
rtaraboo <rtarabocchia@gmail.com>
2015-10-19 14:09:40 -0400

    Deli Counter Excercise Complete

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -6,14 +6,88 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    // Override point for customization after application launch.
+    
+    
+    NSMutableArray  *deliLine = [[NSMutableArray alloc] initWithArray:@[@"Anita", @"Alan", @"Ada", @"Aaron", @"Alan"]];
+    
+    NSMutableArray *deliLine2 = [[NSMutableArray alloc] initWithArray:@[@"Joe",@"Mark"]];
+    
+
+    NSString * greeting1 = [self stringWithDeliLine:deliLine];
+    
+    NSLog(@"%@", greeting1);
+    
+    NSString * greeting2 = [self stringWithDeliLine:deliLine2];
+    
+    NSLog(@"%@", greeting2);
+    
+    
+    
+
+    
     return YES;
 }
 
-/*
- 
- *  Define your methods here!
- 
- */
+
+
+
+
+
+
+
+
+-(NSString *)stringWithDeliLine:(NSMutableArray *)deliLine; {
+    
+    NSMutableString *lineGreeting = [[NSMutableString alloc] init];
+    
+    if (deliLine.count == 0 ) {
+        [lineGreeting appendString:@"The line is currently empty."];
+    
+    } else {
+    
+    
+    [lineGreeting appendString:@"The line is:"];
+        
+        for (NSUInteger i = 0; i < [deliLine count]; i++) {
+            
+            NSUInteger lineNumber = i + 1;
+            
+            NSString *lineNumberString = [NSString stringWithFormat:@"\n%lu. ", lineNumber];
+            
+            [lineGreeting appendString:lineNumberString];
+            [lineGreeting appendString:deliLine[i]];
+            
+            
+        }
+        
+    }
+
+    
+
+    return lineGreeting;
+    
+}
+
+-(void)addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine; {
+    
+    
+    [deliLine addObject:name];
+    
+}
+
+-(NSString *)serveNextCustomerInDeliLine:(NSMutableArray *)deliLine; {
+    
+    NSString *currentCustomer = [NSString stringWithString:deliLine[0]];
+    
+    [deliLine removeObjectAtIndex:0];
+        
+    NSString *nextCustomer = deliLine[0];
+        
+    return currentCustomer;
+    return nextCustomer;
+    
+    
+    
+}
 
 @end
