849cc7b5eda616233529df2404642378aab19e94
richardgmartin <richardgeorgemartin@gmail.com>
2015-11-05 12:47:45 -0500

    completed deli counter exercises

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -10,10 +10,55 @@
     return YES;
 }
 
-/*
- 
- *  Define your methods here!
- 
- */
+// define method(s) implementations
+
+- (NSString *)stringWithDeliLine:(NSArray *)deliLine {
+    
+    // determine if deliLine is empty
+    
+    NSUInteger lineCount = [deliLine count];
+    NSString *peopleInLine = @"";
+    
+    if (lineCount == 0) {
+        NSString *lineIsEmpty = @"The line is currently empty.";
+        return lineIsEmpty;
+    } else {
+        
+       for (NSInteger i = 0; i < lineCount; i++) {
+           
+           NSUInteger personNumber = i + 1;
+           
+           if (personNumber < lineCount) {
+               peopleInLine = [peopleInLine stringByAppendingFormat:@"%lu. %@\n", personNumber, deliLine[i]];
+           } else {
+               peopleInLine = [peopleInLine stringByAppendingFormat:@"%lu. %@", personNumber, deliLine[i]];
+           }
+        }
+    }
+    
+    NSString *theLineIs = @"The line is:\n";
+    NSString *lineOutput = [theLineIs stringByAppendingString:peopleInLine];
+    
+    return lineOutput;
+}
+
+- (void)addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine {
+    
+    // add the submitted <name> to the <deliLine> mutable array
+    
+    [deliLine addObject:name];
+    
+}
+
+- (NSString *)serveNextCustomerInDeliLine:(NSMutableArray *)deliLine {
+    
+    // save 1st name in nsstring; remove 1st object from deliLine; return name
+    
+    NSString *firstInLine = deliLine[0];
+    
+    [deliLine removeObjectAtIndex:0];
+    
+    return firstInLine;
+}
 
 @end
