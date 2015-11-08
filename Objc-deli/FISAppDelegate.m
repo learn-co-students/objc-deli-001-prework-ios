ad00cc808033cb948b829378937dda530db8cdc8
ariingber <ariingber@gmail.com>
2015-11-08 00:48:10 -0500

    all tests but last working

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -4,16 +4,40 @@
 
 @implementation FISAppDelegate
 
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
     return YES;
+    
 }
 
-/*
- 
- *  Define your methods here!
- 
- */
+- (NSString *) stringWithDeliLine: (NSArray *) deliLine {
+    NSMutableString *names = [[NSMutableString alloc] init];
+    if ([deliLine count] == 0){
+        [names appendString: @"The line is currently empty."];
+    }
+    else {
+        [names appendString: (@"The line is:")];
+        for (NSUInteger i = 0; i < [deliLine count]; i++) {
+            [names appendFormat:@"\n%lu. %@", (i+1), deliLine[i]];
+        }
+    }
+    return names;
+}
+
+- addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine{
+    [deliLine addObject:name];
+    return deliLine;
+}
+
+
+
+- (NSString *) serveNextCustomerInDeliLine:(NSMutableArray *)deliLine {
+    NSString *nextCoustomer = deliLine[0];
+    [deliLine removeObjectAtIndex:0];
+    return nextCoustomer;
+    
+}
 
 @end
