704762c9bb99c7e32687d87d80a89abccdad423e
ariingber <ariingber@gmail.com>
2015-11-09 13:52:58 -0500

    Disabled breakpoint and code running correctly

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -36,12 +36,12 @@
 - (NSString *) serveNextCustomerInDeliLine:(NSMutableArray *)deliLine {
     NSString *nextCoustomer = deliLine[0];
     [deliLine removeObjectAtIndex:0];
-    if ([deliLine count] == 0){
-        nextCoustomer = @"The line is currently empty.";
-    }
-    else {
-        nextCoustomer = deliLine[0];
-        }
+    //if ([deliLine count] == 0){
+      //  nextCoustomer = @"The line is currently empty.";
+    //}
+    //else {
+      //  nextCoustomer = deliLine[0];
+        //}
     return nextCoustomer;
     }
 
