afca8f018c14e6492df9bb5f9e64b09676ade47b
mariyaeggs <mariyawilson@att.net>
2015-11-16 08:39:24 -0500

    First commit for Deli Counter lab

--- Objc-deli/FISAppDelegate.h
+++ Objc-deli/FISAppDelegate.h
@@ -2,14 +2,22 @@
 
 #import <UIKit/UIKit.h>
 
+
 @interface FISAppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
+
+@property(nonatomic, copy) NSString *addName;
 
 /*
  
  *  Declare your methods here!
  
  */
+
+-(NSString *)stringWithDeliLine:(NSMutableArray *)deliLine; //deliLine
+-(NSMutableArray *)addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine; //deliLine2
+//-(NSMutableArray toDeliLine:(NSMutableArray *)deliLine;
+-(NSString *)serveNextCustomerInDeliLine:(NSMutableArray *)deliLine; 
 
 @end
