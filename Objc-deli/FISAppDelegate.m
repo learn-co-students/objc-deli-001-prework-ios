1d1d67ed732ea4d59ad9261af6c761d7b6614099
bhtran <bhtran00@gmail.com>
2015-11-22 20:12:09 -0500

    Deli Counter incomplete

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -10,10 +10,28 @@
     return YES;
 }
 
-/*
- 
- *  Define your methods here!
- 
- */
+-(NSString *)stringWithDeliLine:(NSArray *)deliLine {
+    NSString *line = @"The line is:";
+    
+    if ([deliLine count] == 0) {
+        NSLog(@"The line is empty.");
+    }
+    else
+    {
+        for (NSUInteger i = 0; i < [deliLine count]; i++)
+        {
+            NSString *announce = [NSString stringWithFormat:@"\n %lu. %@.", i++, deliLine[i]];
+            line = [line stringByAppendingString:announce];
+        }
+    }
+    
+    return  line;
+}
+-(NSString *)addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine {
+    return nil;
+}
+-(NSString *)serveNextCustomerInDeliLine:(NSMutableArray *)deliLine {
+    return nil;
+}
 
 @end
