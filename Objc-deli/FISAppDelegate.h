8244777d01867674dec13db1000800756b586833
florenciagarcia <floreli18@yahoo.com>
2015-11-24 12:30:20 -0500

    working with methods

--- Objc-deli/FISAppDelegate.h
+++ Objc-deli/FISAppDelegate.h
@@ -6,10 +6,17 @@
 
 @property (strong, nonatomic) UIWindow *window;
 
-/*
- 
- *  Declare your methods here!
- 
+/*Instance methods
+ - instance method indicator
+ () return type
+ * object variables have it
+ method name:argument
  */
+
+-(NSString *)stringWithDeliLine:(NSArray *)deliLine; //accepts an NSArray argument deliLine and returns an NSString object.
+
+-(void)addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine; //accepts an NSString called name and an NSMutableArray called deliLine as arguments, and returns nothing.
+
+-(NSString *)serveNextCustomerInDeliLine:(NSMutableArray *)deliLine; //accepts an NSMutableArray called deliLine as an argument and returns an NSString.
 
 @end
--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -10,10 +10,33 @@
     return YES;
 }
 
-/*
- 
- *  Define your methods here!
- 
- */
+-(NSString *)stringWithDeliLine:(NSArray *)deliLine {
+    NSString *peopleInLine = @"The line is";
+    
+    if ([deliLine count] == 0) { //if no customers in line
+        NSString *noCustomers = @"The line is currently empty.";
+        return noCustomers;
+        
+    } else {
+        
+        for (NSUInteger i = 0; i < [deliLine count]; i++){
+            NSString *listOfCustomers = [NSString stringWithFormat:@"\n%lu. %@.", i++, deliLine[i]]; //format beginning with their number in the queue
+            peopleInLine = [peopleInLine stringByAppendingString:listOfCustomers]; //appending every customer in the queue
+        }
+    }
+    
+    return  peopleInLine;
+}
+
+-(void)addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine {
+    [deliLine addObject:name]; //add the submitted name to the deliLine
+}
+
+-(NSString *)serveNextCustomerInDeliLine:(NSMutableArray *)deliLine {
+    NSString *nextInLine = deliLine[0]; //save the first name in the deliLine to an NSString object.
+    [deliLine removeObjectAtIndex:0]; //remove the first object from the deliLine and return the removed name.
+    
+    return nextInLine;
+}
 
 @end
