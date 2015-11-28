99a9c6ced53e1f0b0887e21556378e61c0301c3d
asbhat <aditya.s.bhat@gmail.com>
2015-11-28 11:32:25 -0500

    deli counter service!

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -10,10 +10,33 @@
     return YES;
 }
 
-/*
- 
- *  Define your methods here!
- 
- */
+- (NSString *)stringWithDeliLine:(NSArray *)deliLine {
+    
+    NSMutableString *deliLineAsString = [[NSMutableString alloc] initWithString:@"The line is:"];
+    NSUInteger serveCounter;
+    
+    if ([deliLine count] == 0) {
+        return @"The line is currently empty.";
+    }
+    
+    for (NSUInteger i = 0; i < [deliLine count]; i++) {
+        serveCounter = i + 1;
+        [deliLineAsString appendFormat:@"\n%lu. %@", (unsigned long)serveCounter, deliLine[i]];
+    }
+    
+    return deliLineAsString;
+}
+
+- (void)addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine {
+    [deliLine addObject:name];
+}
+
+- (NSString *)serveNextCustomerInDeliLine:(NSMutableArray *)deliLine {
+    
+    NSString *nextCustomer = deliLine[0];
+    [deliLine removeObjectAtIndex:0];
+    
+    return nextCustomer;
+}
 
 @end
