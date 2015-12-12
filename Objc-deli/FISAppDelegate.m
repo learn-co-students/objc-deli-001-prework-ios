946bd26c46adf984e28914379e86cca663321b24
Angie-Huang <angelay.huang@yahoo.com>
2015-12-12 18:58:52 -0500

    completed Deli Counter

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -10,10 +10,33 @@
     return YES;
 }
 
-/*
- 
- *  Define your methods here!
- 
- */
+- (NSString*)stringWithDeliLine:(NSArray*)deliLine;
+{
+    NSString *deliCurrentStatus;
 
+    if ([deliLine count] == 0) {
+        deliCurrentStatus = [NSString stringWithFormat:@"The line is currently empty."];
+    } else {
+        deliCurrentStatus = @"The line is:";
+        
+        for (NSUInteger i = 0; i < [deliLine count]; i++) {
+            NSUInteger deliLineNumber = i + 1;
+            NSString *customerQuery= [NSString stringWithFormat:@"\n%lu. %@",deliLineNumber,deliLine[i]];
+            deliCurrentStatus = [deliCurrentStatus stringByAppendingString:customerQuery];
+        }
+    }
+    return deliCurrentStatus;
+}
+
+- (void)addName:(NSString*)name toDeliLine:(NSMutableArray*)deliLine;
+{
+    [deliLine addObject: name];
+}
+
+- (NSString*)serveNextCustomerInDeliLine:(NSMutableArray*)deliLine;
+{
+    NSString* firstInLine = deliLine[0];
+    [deliLine removeObjectAtIndex:0];
+    return firstInLine;
+}
 @end
