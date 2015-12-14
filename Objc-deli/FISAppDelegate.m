024256628c5a49b2fa908a632975046341064840
Dnld <steinert.donald@gmail.com>
2015-12-14 10:36:27 -0500

    completes deli counter

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -16,4 +16,31 @@
  
  */
 
+- (NSString *)stringWithDeliLine:(NSArray *)deliLine
+{
+    if ([deliLine count] > 0) {
+        NSMutableString *deliLineAsString = [@"The line is:" mutableCopy];
+        for (NSUInteger i = 0; i < [deliLine count]; i++) {
+            [deliLineAsString appendFormat:@"\n%li. %@", (i + 1), deliLine[i]];
+        }
+        NSLog(@"%@", deliLineAsString);
+        return deliLineAsString;
+    } else {
+        return @"The line is currently empty.";
+    }
+}
+
+- (void)addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine
+{
+    [deliLine addObject:name];
+}
+
+
+- (NSString *)serveNextCustomerInDeliLine:(NSMutableArray *)deliLine
+{
+    NSString *nextCustomer = deliLine[0];
+    [deliLine removeObjectAtIndex:0];
+    return nextCustomer;
+}
+
 @end
