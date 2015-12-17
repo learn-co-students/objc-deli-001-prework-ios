017ac862581f80ee2c9ce44a2bc6412e2a775db1
AliMar11 <alicia.marisal@gmail.com>
2015-12-16 22:58:47 -0500

    happy deli (all tests passed)

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -16,4 +16,46 @@
  
  */
 
+- (NSString *) stringWithDeliLine: (NSArray *) deliLine; {
+   
+    NSMutableString *lineOfPeeps = [[NSMutableString alloc]init];
+    if ([deliLine count] == 0){
+        lineOfPeeps = [@"The line is currently empty." mutableCopy];
+        
+} else {
+    // num is more than zero
+    lineOfPeeps = [@"The line is:" mutableCopy];
+    
+    for (NSUInteger i = 0; i < [deliLine count]; i++) {
+       // original line [lineOfPeeps appendFormat: @"%@ \n %lu. %@", i + 1, deliLine[i]];
+        // no errors line [lineOfPeeps appendFormat: @"\n %lu %@", i + 1, deliLine[i]];
+        [lineOfPeeps appendFormat: @"\n%lu. %@", i + 1, deliLine[i]];
+    }
+  }
+
+    return lineOfPeeps;
+}
+
+- (void *)addName: (NSString*)name toDeliLine: (NSMutableArray*)deliLine;{
+   
+    if (name > 0) {
+        [deliLine addObject: name];
+    }
+    
+    return nil;
+}
+
+
+
+- (NSString*)serveNextCustomerInDeliLine:(NSMutableArray*)deliLine;{
+
+    
+    [deliLine removeObjectAtIndex:0];
+    
+    return deliLine;
+
+}
+
+
+
 @end
