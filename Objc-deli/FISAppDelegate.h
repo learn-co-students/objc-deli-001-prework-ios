99fd4dd5bbd1c712d4202a8642ed125f93a4d5a9
florenciagarcia <floreli18@yahoo.com>
2015-12-21 15:32:25 -0500

    Initialize NSMutableString

--- Objc-deli/FISAppDelegate.h
+++ Objc-deli/FISAppDelegate.h
@@ -13,10 +13,11 @@
  method name:argument
  */
 
--(NSString *)stringWithDeliLine:(NSArray *)deliLine; //accepts an NSArray argument deliLine and returns an NSString object.
+- (NSString *)stringWithDeliLine:(NSArray *)deliLine; //accepts an NSArray argument deliLine and returns an NSString object.
 
--(void)addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine; //accepts an NSString called name and an NSMutableArray called deliLine as arguments, and returns nothing.
+- (void)addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine; //accepts an NSString called name and an NSMutableArray called deliLine as arguments, and returns nothing.
 
--(NSString *)serveNextCustomerInDeliLine:(NSMutableArray *)deliLine; //accepts an NSMutableArray called deliLine as an argument and returns an NSString.
+- (NSString *)serveNextCustomerInDeliLine:(NSMutableArray *)deliLine; //accepts an NSMutableArray called deliLine as an argument and returns an NSString.
 
 @end
+
--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -10,31 +10,35 @@
     return YES;
 }
 
--(NSString *)stringWithDeliLine:(NSArray *)deliLine {
-    NSString *peopleInLine = @"The line is";
+- (NSString *)stringWithDeliLine:(NSArray *)deliLine {
+    NSMutableString *peopleInLine = [[NSMutableString alloc]init];
     
     if ([deliLine count] == 0) { //if no customers in line
-        NSString *noCustomers = @"The line is currently empty.";
-        return noCustomers;
+        peopleInLine = [@"The line is currently empty." mutableCopy];
         
     } else {
         
-        for (NSUInteger i = 0; i < [deliLine count]; i++){
-            NSString *listOfCustomers = [NSString stringWithFormat:@"\n%lu. %@.", i++, deliLine[i]]; //format beginning with their number in the queue
-            peopleInLine = [peopleInLine stringByAppendingString:listOfCustomers]; //appending every customer in the queue
+        peopleInLine = [@"The line is:" mutableCopy];
+        
+        for (NSUInteger i = 0; i < [deliLine count]; i++){ //for loop counts the number of elements in the array at run time
+            [peopleInLine appendFormat: @"\n%lu. %@.", i + 1, deliLine[i]]; //format beginning with their number in the queue
         }
     }
+        /*
+            NSString *listOfCustomers = [NSString stringWithFormat:@"\n%lu. %@.", i++, deliLine[i]]; //format beginning with their number in the queue
+            peopleInLine = [peopleInLine stringByAppendingString:listOfCustomers]; //appending every customer in the queue
+        */
     
     return  peopleInLine;
 }
 
--(void)addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine {
-    [deliLine addObject:name]; //add the submitted name to the deliLine
-}
+- (void)addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine {
+    [deliLine addObject:name]; //add the submitted name to the end of deliLine (the mutable array)
+} //void type methods have no return to capture
 
--(NSString *)serveNextCustomerInDeliLine:(NSMutableArray *)deliLine {
-    NSString *nextInLine = deliLine[0]; //save the first name in the deliLine to an NSString object.
-    [deliLine removeObjectAtIndex:0]; //remove the first object from the deliLine and return the removed name.
+- (NSString *)serveNextCustomerInDeliLine:(NSMutableArray *)deliLine {
+    NSString *nextInLine = deliLine[0]; //save the first name in the deliLine to an NSString object
+    [deliLine removeObjectAtIndex:0]; //remove the first object from the deliLine and return the removed name
     
     return nextInLine;
 }
