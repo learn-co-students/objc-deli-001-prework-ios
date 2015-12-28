9a54a04b7e07e9cb949c5429320306a2cd3d4197
magfurulabeer <magfurulabeer@gmail.com>
2015-12-28 15:02:07 -0500

    Complete program

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -16,4 +16,32 @@
  
  */
 
+- (NSString *)stringWithDeliLine:(NSArray *)deliLine {
+    
+    if( [deliLine count] == 0 ) {
+        return @"The line is currently empty.";
+    }
+    
+    NSMutableArray *formattedDeliLine = [[NSMutableArray alloc] init];
+    
+    for (NSUInteger i = 0; i < [deliLine count]; i++) {
+        formattedDeliLine[i] = [NSString stringWithFormat:@"%lu. %@", i+1, deliLine[i]];
+    }
+    
+    NSMutableString *deliLineString = [@"The line is:\n" mutableCopy];
+    [deliLineString appendString:[formattedDeliLine componentsJoinedByString:@"\n"]];
+    
+    return deliLineString;
+}
+
+- (void)addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine {
+    [deliLine addObject:name];
+}
+
+- (NSString *)serveNextCustomerInDeliLine:(NSMutableArray *)deliLine {
+    NSString *nextCustomer = deliLine[0];
+    [deliLine removeObjectAtIndex:0];
+    return nextCustomer;
+}
+
 @end
