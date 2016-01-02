ccae49bb45dcf948390a57b07a18af02c88cfb6e
jeremyfeld <jeremyfeld@gmail.com>
2016-01-01 21:39:26 -0500

    methods for deli line, add to line serve next

--- Objc-deli/FISAppDelegate.h
+++ Objc-deli/FISAppDelegate.h
@@ -12,4 +12,8 @@
  
  */
 
+- (NSString *)stringWithDeliLine:(NSArray *)deliLine;
+- (NSString *)addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine;
+- (NSString *)serveNextCustomerInDeliLine:(NSMutableArray *)deliLine;
+
 @end
