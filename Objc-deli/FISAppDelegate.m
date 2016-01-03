3b47f31199abc2f8c958f4940dbd929d9fe14e32
anmolbajajnet <hello@anmolbajaj.net>
2016-01-02 21:52:18 -0800

    solve lab. is  hard.

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -16,4 +16,51 @@
  
  */
 
+
+
+- (NSString *)stringWithDeliLine:(NSArray *)deliLine; {
+    
+    NSString *lineResult;
+    
+    if (![deliLine count]){
+        lineResult = @"This line is currently empty.";
+    } else {
+        lineResult = @"The line is:";
+        
+        for (NSUInteger i = 0; i < [deliLine count]; i++) {
+    lineResult = [lineResult stringByAppendingFormat:@"n%lu. %@", i+1, deliLine[i]];
+            
+        }
+    }
+    
+    return lineResult;
+}
+
+- (void)addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine; {
+    
+    [deliLine addObject:name];
+    
+    
+    return;
+}
+
+- (NSString *)serveNextCustomerInDeliLine: (NSMutableArray *)deliLine; {
+    
+    BOOL customerServed = NO;
+    NSString *firstcustomer;
+    
+    
+    for (NSUInteger i = 0; i < [deliLine count]&&!customerServed; i++) {
+        if (deliLine[i]) {
+            customerServed = YES;
+            firstcustomer = deliLine[i];
+            [deliLine removeObjectAtIndex:i];
+            
+        }
+    }
+    
+    return firstcustomer;
+}
+
+
 @end
