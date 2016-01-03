d5d47ecfa1a4992b196fda1516b0f339c47dbef6
fleish007 <cfleish@gmail.com>
2016-01-02 22:09:05 -0500

    Deli counter exercise

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -16,4 +16,41 @@
  
  */
 
+- (NSString *)stringWithDeliLine:(NSArray *)deliLine {
+    NSString *deliLineResult;
+    
+    if (![deliLine count]) {
+        deliLineResult = @"The line is currently empty.";
+    } else {
+        deliLineResult = @"The line is:";
+        
+        for (NSUInteger i = 0; i < [deliLine count]; i++) {
+            deliLineResult = [deliLineResult stringByAppendingFormat:@"\n%lu. %@", i+1, deliLine[i]];
+        }
+    }
+    
+    return deliLineResult;
+}
+
+- (void)addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine {
+    [deliLine addObject:name];
+    
+    return;
+}
+
+- (NSString *)serveNextCustomerInDeliLine:(NSMutableArray *)deliLine {
+    BOOL customerServed = NO;
+    NSString *firstCustomer;
+    
+    for (NSUInteger i =0; i < [deliLine count]&&!customerServed; i++) {
+        if (deliLine[i]) {
+            customerServed = YES;
+            firstCustomer = deliLine[i];
+            [deliLine removeObjectAtIndex:i];
+        }
+    }
+    
+    return firstCustomer;
+}
+
 @end
