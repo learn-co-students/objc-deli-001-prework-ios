d4cd368364659a2929372c269a3e0f2647b489a9
bclous <brian.clouser@gmail.com>
2016-01-11 11:32:09 -0500

    completed deli lab all tests passed

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -16,4 +16,55 @@
  
  */
 
+- (NSString *)stringWithDeliLine:(NSArray *)deliLine {
+   
+    NSUInteger lineLength = [deliLine count];
+    BOOL lineEmpty = lineLength == 0;
+    NSMutableString *lineQueue = [[NSMutableString alloc] init];
+    
+
+    if(lineEmpty)
+    {
+        [lineQueue appendString:@"The line is currently empty."];
+    }
+    else
+    {
+        NSMutableString *linePlace = [[NSMutableString alloc] init];
+        
+        for(NSUInteger i = 0; i < lineLength; i++)
+        {
+            NSInteger lineSpot = i+1;
+            NSString *nameOnLine = deliLine[i];
+            
+            if(i == lineLength - 1)
+            {
+                [linePlace appendFormat:@"%lu. %@", lineSpot, nameOnLine];
+            }
+            else
+            {
+                [linePlace appendFormat:@"%lu. %@\n", lineSpot, nameOnLine];
+            }
+        }
+        
+        [lineQueue appendFormat:@"The line is:\n%@", linePlace];
+    }
+    
+    return lineQueue;
+}
+
+- (void)addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine {
+    
+    [deliLine addObject:name];
+ 
+}
+
+- (NSString *)serveNextCustomerInDeliLine:(NSMutableArray *)deliLine {
+
+        NSString *nextCustomer = [deliLine objectAtIndex:0];
+    [deliLine removeObjectAtIndex:0];
+    return nextCustomer;
+    
+    
+}
+
 @end
