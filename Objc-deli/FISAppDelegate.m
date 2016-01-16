fe24e997866f1f877c89a4f4c4283fde1afc04ad
zelosa <wanda.yanli@gmail.com>
2016-01-15 20:18:46 -0500

    deli

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -10,6 +10,37 @@
     return YES;
 }
 
+-(NSString *)stringWithDeliLine:(NSArray *)deliLine{
+    NSMutableString *line  = [@"" mutableCopy];
+    
+    if ([deliLine count] <= 0)
+    {
+        [line appendString: @"The line is currently empty."];
+    }
+    else
+    {
+        [line appendString: @"The line is:"];
+        //NSMutableString *line  = [@"The line is:" mutableCopy];
+        
+        for (NSUInteger i = 0; i < [deliLine count]; i++){
+            
+            [line appendFormat:@"\n%lu. %@", i+1, deliLine[i]];
+         }
+    }
+        return line;
+}
+    
+    -(void)addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine;{
+        [deliLine addObject:name];
+    }
+    
+    -(NSString *)serveNextCustomerInDeliLine:(NSMutableArray *)deliLine{
+        
+        NSString *firstname = deliLine[0];
+        [deliLine removeObjectAtIndex:0];
+        return firstname;
+    }
+
 /*
  
  *  Define your methods here!
