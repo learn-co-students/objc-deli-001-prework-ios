34c8110c13dcb4b25a325513a976795b39352c8e
Ryan Othman <ryhoth@gmail.com>
2016-01-21 07:36:50 -0500

    deli counter lab with errors

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -6,14 +6,37 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    // Override point for customization after application launch.
+    return nil;
     return YES;
 }
 
-/*
- 
- *  Define your methods here!
- 
- */
+- (NSString *)stringWithDeliLine:(NSArray *)deliLine; {
+    if ([deliLine count] == 0) {
+        NSLog(@"The line is currently empty.");
+    }
+    else {
+              NSLog(@"The line is:");
+        for  (NSUInteger i = 0 ;i< [deliLine count] ; i++) {
+                  NSUInteger lineOrder = i+1;
+                  NSString *printDeliLine = [NSString stringWithFormat:@"%lu. %@",lineOrder, deliLine[i]];
+                  NSLog(@"%@", printDeliLine);
+                  return printDeliLine;
+        } ;
+        }};
+
+    
+- (void) ToDeliLine:(NSString *)name :(NSMutableArray *)deliLine; {
+    [deliLine addObject: name];
+};
+
+    
+- (NSString *)serveNextCustomerInDeliLine:(NSMutableArray *)deliLine; {
+        for (NSUInteger i = 0; i < [deliLine count]; i++ ) {
+            NSString *savedString = deliLine[i];
+            [deliLine removeObjectAtIndex: i];
+            return savedString;
+        };
+};
+
 
 @end
