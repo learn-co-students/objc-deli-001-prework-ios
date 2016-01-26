4accc3731b725fc25092bcf967b60ea5d8988893
Billy <billylan22@gmail.com>
2016-01-25 18:50:13 -0800

    deli counter methods working and tests passing

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -16,4 +16,30 @@
  
  */
 
+- (NSString *)stringWithDeliLine:(NSArray *)deliLine {
+    NSString *deliLineString = @"The line is:";
+    
+    if ([deliLine count]) {
+        for (NSUInteger i = 0; i < [deliLine count]; i ++) {
+            NSUInteger deliLineOrder = i + 1;
+            deliLineString = [deliLineString stringByAppendingFormat:@"\n%lu. %@", deliLineOrder, deliLine[i]];
+        }
+    } else {
+        deliLineString = @"The line is currently empty.";
+    }
+    
+    return deliLineString;
+}
+
+- (void)addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine {
+    return [deliLine addObject:name];
+}
+
+- (NSString *)serveNextCustomerInDeliLine:(NSMutableArray *)deliLine {
+    NSUInteger firstInLine = 0;
+    NSString *nextCustomer = deliLine[firstInLine];
+    [deliLine removeObjectAtIndex:(firstInLine)];
+    return nextCustomer;
+}
+
 @end
