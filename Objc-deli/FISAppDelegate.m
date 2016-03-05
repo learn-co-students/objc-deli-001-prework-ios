d75f92ad73b9d68dde4b0ea017119dbca1c58cc1
Uskinn <s.nevzor@yahoo.com>
2016-03-05 16:41:11 -0500

    Commit

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -6,14 +6,59 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    // Override point for customization after application launch.
+    // Overrid point for customization after application launch.
+   
+    
+    
+    
+    
+    
     return YES;
 }
-
 /*
  
  *  Define your methods here!
  
  */
+-(NSString *)stringWithDeliLine:(NSArray*)deliLine {
+
+    NSMutableString *mDeliLine = [[NSMutableString alloc] init];
+    
+    if ([deliLine count] == 0) {
+        
+        NSString *noLine = @"The line is currently empty";
+        
+        return noLine;
+        
+    } else
+        
+        [mDeliLine appendString: @"The line is: \n"];
+        
+    for (NSUInteger i = 0; i < [deliLine count]; i++) {
+            
+            NSUInteger numberInLine = i + 1;
+            
+            [mDeliLine appendFormat:@"\n%lu. %@", numberInLine, deliLine[i]];
+             
+    }
+        
+    return mDeliLine;
+}
+    
+
+
+-(void *)addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine{
+    
+    [deliLine addObject:name];
+    return nil;
+}
+
+-(NSString *)serveNextCustomerInDeliLine:(NSMutableArray *)deliLine {
+    
+    NSString *nextCustomer = deliLine[0];
+    [deliLine removeObjectAtIndex:0];
+    
+    return nextCustomer;
+}
 
 @end
