9fc0267bb748f8c3e49a9a9e86bb94d127c7efc4
yayitserica <ericamillado@gmail.com>
2016-03-23 12:04:21 -0400

    tests passed deli line

--- Objc-deli/FISAppDelegate.h
+++ Objc-deli/FISAppDelegate.h
@@ -11,5 +11,10 @@
  *  Declare your methods here!
  
  */
+-(NSString *)stringWithDeliLine:(NSMutableArray *)deliLine;
+
+-(NSMutableArray *)addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine;
+
+-(NSString *) serveNextCustomerInDeliLine:(NSMutableArray *)deliLine;
 
 @end
--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -16,4 +16,37 @@
  
  */
 
+
+
+-(NSString*) stringWithDeliLine:(NSMutableArray *)deliLine{
+    NSString *deliLineList = @"The line is:\n";
+    if ([deliLine count]==0) {
+        return @"The line is currently empty.";
+    } else {
+        for (NSInteger i=0; i<[deliLine count]; i++) {
+            if (i!=[deliLine count]-1) {
+                deliLineList=[deliLineList stringByAppendingString:[NSString stringWithFormat: @"%li. %@\n",i+1,deliLine[i]]];
+            } else {
+                deliLineList=[deliLineList stringByAppendingString:[NSString stringWithFormat: @"%li. %@",i+1,deliLine[i]]];
+            }
+            
+        }
+            return deliLineList;
+    }
+}
+
+
+
+-(NSMutableArray *)addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine {
+    [deliLine addObject:name];
+    return deliLine;
+}
+
+-(NSString *)serveNextCustomerInDeliLine:(NSMutableArray *)deliLine {
+    NSString *firstCustomer = deliLine[0];
+    [deliLine removeObjectAtIndex:0];
+    return firstCustomer;
+        
+}
+
 @end
