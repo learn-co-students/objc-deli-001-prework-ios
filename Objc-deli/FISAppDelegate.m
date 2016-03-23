0db0af02c6b1dc28e5a1eab5870d6e45c750db39
imryan <notryancohen@gmail.com>
2016-03-23 12:59:44 -0400

    completed lesson, added required methods

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -4,16 +4,38 @@
 
 @implementation FISAppDelegate
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
     return YES;
 }
 
-/*
- 
- *  Define your methods here!
- 
- */
+- (NSString *)stringWithDeliLine:(NSArray *)deliLine {
+    NSMutableString *customerListString = [NSMutableString stringWithString:@"The line is:"];
+    
+    if (deliLine.count == 0) {
+        return @"The line is currently empty.";
+    }
+    
+    for (int i = 0; i < deliLine.count; i++) {
+        NSString *customer = [NSString stringWithFormat:@"\n%d. %@", i+1, deliLine[i]];
+        [customerListString appendString:customer];
+    }
+    
+    return customerListString;
+}
+
+- (void)addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine {
+    if (deliLine != nil) {
+        [deliLine addObject:name];
+    }
+}
+
+- (NSString *)serveNextCustomerInDeliLine:(NSMutableArray *)deliLine {
+    NSString *firstCustomerName = [deliLine objectAtIndex:0];
+    [deliLine removeObjectAtIndex:0];
+    
+    return firstCustomerName;
+}
 
 @end
