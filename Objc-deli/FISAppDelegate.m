73a1f691aa6a7797eb169dccacabbaee5811c8d6
user116796 <gyk42@hotmail.com>
2016-03-31 20:30:09 -0400

    Deli counter

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -16,4 +16,36 @@
  
  */
 
+- (NSString *)stringWithDeliLine:(NSArray *)deliLine {
+    
+    NSString *lineIs = @"The line is:";
+    
+    if ([deliLine count] == 0) {
+        lineIs = @"The line is currently empty.";
+    } else {
+        for (NSUInteger i = 0; i < [deliLine count]; i++) {
+            NSUInteger customerNumber = i + 1;
+            lineIs = [lineIs stringByAppendingFormat: @"\n%lu. %@", customerNumber, deliLine[i]];
+        }
+    }
+    
+    return lineIs;
+}
+
+- addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine {
+    
+    [deliLine addObject:name];
+    
+    return deliLine;
+}
+
+- (NSString *)serveNextCustomerInDeliLine:(NSMutableArray *)deliLine {
+    
+    NSString *firstName = deliLine[0];
+    
+    [deliLine removeObjectAtIndex:0];
+    
+    return firstName;
+}
+
 @end
