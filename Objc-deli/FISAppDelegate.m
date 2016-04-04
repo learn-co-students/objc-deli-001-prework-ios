d3a6a79e4f050b3868f4915cdab00637a130cfd2
hdinhof1 <hdinhofer@gmail.com>
2016-04-04 13:56:50 -0400

    All tests are successful

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -15,5 +15,32 @@
  *  Define your methods here!
  
  */
+-(NSString *)stringWithDeliLine:(NSArray *)deliLine{
+    NSString *message = @"";
+    NSUInteger placeInLine = 0;
+    if (deliLine.count < 1) {
+        message = @"The line is currently empty.";
+    }else {
+        message = @"The line is:";
+        for (NSUInteger i = 0; i<deliLine.count; i++) {
+            placeInLine = i+1; //or placeInLine++
+            message = [message stringByAppendingFormat:@"\n%li. %@", placeInLine, deliLine[i]]; //overwrites to ONLY LAST VALUE?
+        }
+    }
+    
+    
+    return message;
+}
+
+-(void)addName:(NSString *)name toDeliLine:(NSMutableArray*)deliLine{
+    [deliLine addObject:name];
+}
+
+-(NSString *)serveNextCustomerInDeliLine:(NSMutableArray *)deliLine{
+  //  if (deliLine.count > 0)
+    NSString *customerToBeServed = deliLine[0];
+    [deliLine removeObjectAtIndex:0];
+    return customerToBeServed;
+}
 
 @end
