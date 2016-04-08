7ce471423eb6f64f0cc4449b82a60c766a51c12d
github username <github email address>
2016-04-08 13:14:47 -0400

    lab completed successfully

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -10,10 +10,31 @@
     return YES;
 }
 
-/*
- 
- *  Define your methods here!
- 
- */
+//returns a string that shows the line
+- (NSString *)stringWithDeliLine:(NSArray *)deliLine {
+    if (![deliLine count]) {
+        return @"The line is currently empty.";
+    }
+    NSMutableString *deliLineString = [[NSMutableString alloc] initWithString:@"The line is:"];
+    for (NSUInteger i =0; i < [deliLine count]; i++) {
+        NSUInteger queueNumber = i + 1;
+        [deliLineString appendFormat:@"\n%lu. %@", queueNumber, deliLine[i]];
+    }
+    return deliLineString;
+}
+
+
+//adds a name to the line
+- (void)addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine {
+    [deliLine addObject:name];
+}
+
+
+//removes the first customer in the line to be served
+- (NSString *)serveNextCustomerInDeliLine:(NSMutableArray *)deliLine {
+    NSString *nextCustomerInLine = deliLine[0];
+    [deliLine removeObjectAtIndex:0];
+    return nextCustomerInLine;
+}
 
 @end
