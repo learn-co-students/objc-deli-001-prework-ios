f541fccd6368ddd25d01b496b61677313f3967a3
hannachoi <hanna.y.choi@gmail.com>
2016-04-17 20:33:23 -0400

    Done.

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -16,4 +16,35 @@
  
  */
 
+- (NSString *)stringWithDeliLine:(NSArray *)deliLine {
+    
+    NSMutableString *deliString = [[NSMutableString alloc]init];
+    
+    if ([deliLine count] == 0) {
+        return @"The line is currently empty.";
+    }
+    else {
+        [deliString appendString:@"The line is:"];
+        for (NSUInteger i = 0; i < [deliLine count]; i++) {
+            NSUInteger queueNumber = i + 1;
+            NSString *personAndNumber = [NSString stringWithFormat:(@"\n%lu. %@"), queueNumber, deliLine[i]];
+            [deliString appendString:personAndNumber];
+        }
+        return deliString;
+    }
+}
+
+- (void)addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine {
+    [deliLine addObject:name];
+}
+
+- (NSString *)serveNextCustomerInDeliLine:(NSMutableArray *)deliLine {
+    
+    NSString *firstInLine = [deliLine firstObject];
+
+    [deliLine removeObjectAtIndex:0];
+    return firstInLine;
+    
+}
+
 @end
