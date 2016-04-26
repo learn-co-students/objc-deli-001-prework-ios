8eb978fef691ce1b7c187f9985c5de0b787ca8b8
jellybeanjohnny <mamerige@icloud.com>
2016-04-26 11:51:54 -0400

    Implemented methods to pass all tests

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -16,4 +16,33 @@
  
  */
 
+- (NSString *)stringWithDeliLine:(NSArray *)deliLine
+{
+  if (deliLine.count == 0) {
+    return @"The line is currently empty.";
+  }
+  
+  NSMutableString *line = [[NSMutableString alloc] initWithString:@"The line is:"];
+  
+  NSUInteger index = 1;
+  for (NSString *name in deliLine) {
+    NSString *numberAndName = [NSString stringWithFormat:@"\n%lu. %@", index, name];
+    [line appendString:numberAndName];
+    index++;
+  }
+  return line;
+}
+
+- (void)addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine
+{
+  [deliLine addObject:name];
+}
+
+- (NSString *)serveNextCustomerInDeliLine:(NSMutableArray *)deliLine
+{
+  NSString *nextCustomer = [deliLine firstObject];
+  [deliLine removeObjectAtIndex:0];
+  return nextCustomer;
+}
+
 @end
