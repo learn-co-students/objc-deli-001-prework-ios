3ceea4acf84be55c46d4371e74f8b38ed91317a5
ugowe <joe@spheares.com>
2016-04-28 13:58:45 -0400

    add code for lab Deli Counter

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -10,10 +10,31 @@
     return YES;
 }
 
-/*
- 
- *  Define your methods here!
- 
- */
+-(NSString *)stringWithDeliLine:(NSArray *)deliLine {
+    
+    NSString *deliGreetingWithLine = @"The line is:";
+
+    if ( [deliLine count] == 0) {
+            return @"The line is currently empty.";
+    }
+    
+    for (NSUInteger i=0; i < [deliLine count]; i++) {
+            deliGreetingWithLine = [deliGreetingWithLine stringByAppendingFormat:@"\n%lu. %@", i + 1, deliLine[i]];
+    }
+    
+    return deliGreetingWithLine;
+    }
+
+-(void)addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine {
+        [deliLine addObject: name];
+        return;
+    }
+
+-(NSString *)serveNextCustomerInDeliLine:(NSMutableArray *)deliLine {
+    
+    NSString *firstPersonInLine = [deliLine objectAtIndex:0];
+    [deliLine removeObjectAtIndex:0];
+    return firstPersonInLine;
+    }
 
 @end
