2702a37ef64d3ff57f4ca41aaadfd061c7fdbac5
mariogabriele1 <mgabriele89@gmail.com>
2016-04-29 09:00:46 -0400

    Deli counter

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -10,10 +10,71 @@
     return YES;
 }
 
-/*
- 
- *  Define your methods here!
- 
- */
+
+
+
+
+//METHODS
+
+- (NSString *)stringWithDeliLine: (NSArray *)deliLine {
+    
+    NSInteger customer = [deliLine count];
+    NSMutableString *returnedString = [NSMutableString stringWithString:@""];
+    
+    if (customer == 0) {
+        [returnedString setString:@"The line is currently empty."];
+    
+    }
+    
+    else {
+         [returnedString setString:@"The line is:\n"];
+        
+        for (NSUInteger i = 0; i < [deliLine count]; i++) {
+            
+            int iCount = i+1;
+            NSString *iCountString = [NSString stringWithFormat:@"%i", iCount];
+            
+            [returnedString appendString:(iCountString)];
+            [returnedString appendString:@". "];
+            [returnedString appendString:(deliLine [i])];
+            if (i < [deliLine count ] - 1) {
+                [returnedString appendString:@"\n"];
+            }
+        }
+    }
+    
+    
+    return returnedString;
+    
+}
+
+
+
+- (void) addName:(NSString *)name toDeliLine:(NSMutableArray *)toDeliLine
+{
+    
+        [toDeliLine addObject:name];
+    
+}
+
+
+
+- (NSString *)serveNextCustomerInDeliLine: (NSMutableArray *) deliLine {
+    NSString *returnedString;
+    
+    returnedString = deliLine [0];
+    [deliLine removeObjectAtIndex: 0];
+    
+    
+    
+    return returnedString;
+    
+}
+
+
+
+
+
+
 
 @end
