48357bb820f38dd128d505dc2b6a5110b11df7de
uArendt <jarendt66@me.com>
2016-05-02 18:52:33 -0400

    updated things

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -9,7 +9,36 @@
     // Override point for customization after application launch.
     return YES;
 }
+-(NSString *) stringWithDeliLine:(NSArray *)deliLine {
+    
+    NSMutableString *lineStatus = [[NSMutableString alloc]init];
+    
+    if (deliLine.count == 0) {
+        [lineStatus appendString:@"The line is empty"];
+        return [NSString stringWithString:lineStatus];
+    }
+    [lineStatus appendString:@"The line is:"];
+    for (NSUInteger i = 1; i< deliLine.count; i++) {
+        //this is because the customer's number starts at 1, not 0
+        NSUInteger customerNumber = i + 1;
+        [lineStatus appendFormat:@"\n%lu. %@", customerNumber, deliLine[i]];
+    }
+    return lineStatus;
+}
 
+-(void) addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine {
+    [deliLine addObject:name];
+}
+
+-(NSString *) serveNextCustomerInDeliLine:(NSMutableArray *)deliLine {
+    NSString *nextInLine = [deliLine firstObject];
+    [deliLine removeObjectAtIndex:0];
+    return nextInLine;
+}
+
+/*
+
+//return a string from an array
 - (NSString *) stringWithDeliLine:(NSArray *) deliLine; {
     
     NSMutableString *lineStatus = [NSMutableString new];
@@ -36,10 +65,25 @@
     return nextInLine;
 }
 
-/*
  
  *  Define your methods here!
  
  */
 
 @end
+
+
+
+//
+//for (int i = 0; i < [items count]; i++) {
+//    NSString *item = [items objectAtIndex:i];
+//    NSLog(@"%@", item);
+//}
+//
+//
+//
+//// For every item in the items array ...
+//for (NSString *item in items) {
+//    // Log the description of item
+//    NSLog(@"%@", item);
+//}
