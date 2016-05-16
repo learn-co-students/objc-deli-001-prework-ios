37ea58054714c2e75e5e2ee6efb87fe645490b67
Erica <erica@Ericas-MacBook-Air.local>
2016-05-15 23:43:54 -0400

    deli-counter

--- Objc-deli/FISAppDelegate.h
+++ Objc-deli/FISAppDelegate.h
@@ -11,5 +11,7 @@
  *  Declare your methods here!
  
  */
-
+- (NSString *)stringWithDeliLine:(NSArray *)deliLine;
+- (void)addName:(NSArray *)name toDeliLine:(NSMutableArray *)deliLine;
+- (NSString *)serveNextCustomerInDeliLine:(NSMutableArray *)deliLine;
 @end
