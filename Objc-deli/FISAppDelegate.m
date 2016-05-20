dff743b8bd4f0b58d15d7950b9e374f053c5f160
Salmaan Rizvi <sar228@cornell.edu>
2016-05-20 11:36:52 -0400

    all tests passed, not showing on learn web interface

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -10,10 +10,35 @@
     return YES;
 }
 
-/*
- 
- *  Define your methods here!
- 
- */
+- (NSString *)stringWithDeliLine:(NSArray *)deliLine {
+    NSMutableString *line = [@"" mutableCopy];
+    if ([deliLine count] == 0) {
+        [line appendFormat:@"The line is currently empty."];
+    }
+    else {
+        [line appendString:@"The line is:"];
+        for(NSUInteger i = 0; i < [deliLine count]; i++) {
+            [line appendFormat:@"\n%lu. %@", i+1, deliLine[i]];
+            NSLog(@"%@", line);
+        }
+    }
+    return line;
+}
+
+- (void)addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine {
+    [deliLine addObject:name];
+    return;
+}
+
+- (NSString *)serveNextCustomerInDeliLine:(NSMutableArray *)deliLine {
+    if([deliLine count] == 0) {
+        return @"";
+    }
+    else {
+        NSString *nextCustomer = deliLine[0];
+        [deliLine removeObjectAtIndex:0];
+        return nextCustomer;
+    }
+}
 
 @end
