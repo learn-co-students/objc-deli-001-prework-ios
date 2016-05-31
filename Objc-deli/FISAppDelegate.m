4714951cce1ad19f327206deaf387c9e857fe99c
mikeflorez23 <mike_florez@ymail.com>
2016-05-30 21:18:19 -0400

    Done.

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -6,14 +6,46 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    // Override point for customization after application launch.
+
+    NSString *nameOfNewCustomer = @"Michael";
+    
+    NSMutableArray *newCustomer = [[NSMutableArray alloc] init];
+    
+    [self addName:nameOfNewCustomer toDeliLine:newCustomer];
+    
     return YES;
 }
 
-/*
- 
- *  Define your methods here!
- 
- */
+-(NSString *) stringWithDeliLine:(NSArray *)deliLine{
 
+    NSString *theLineIs = @"The line is:";
+    
+    if ([deliLine count] == 0) {
+        
+        return @"The line is currently empty.";
+        
+    } else {
+        
+        for (NSUInteger index = 0; index < deliLine.count; index++) {
+            
+            NSString *customers = [NSString stringWithFormat:@"\n %lu %@ ",
+                                   index+1, deliLine[index]];
+            theLineIs = [theLineIs stringByAppendingString:customers];
+        }
+        return theLineIs;
+    }
+}
+
+-(void) addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine {
+    [deliLine addObject:name];
+}
+-(NSString *) serveNextCustomerInDeliLine:(NSMutableArray *)deliLine {
+    if (deliLine.count == 0) {
+        return nil;
+    }else{
+    NSString *firstObject = deliLine[0];
+    [deliLine removeObject:firstObject];
+    return firstObject;
+    }
+}
 @end
