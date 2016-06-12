31078a8e51608da188382be35a4e05c7a502ab99
sejdemi <sejdemi@gmail.com>
2016-06-12 16:34:39 -0400

    updated

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -10,10 +10,37 @@
     return YES;
 }
 
-/*
+-(NSString *)stringWithDeliLine:(NSArray *)deliLine {
+    
+    NSString *linedeclaration = @"The line is:";
+    
+    if ([deliLine count] == 0) {
+        return @"The line is currently empty.";
+    }
+    
+        else {
  
- *  Define your methods here!
- 
- */
+            for (NSUInteger i = 0; i < deliLine.count; i++) {
+                
+NSString *customers = [NSString stringWithFormat:@"\n %lu %@ ", i+1, deliLine[i]];
+linedeclaration = [linedeclaration stringByAppendingString:customers];
+}
+return linedeclaration;
+        }
+    }
+
+-(void) addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine {
+       [deliLine addObject:name];
+    }
+-(NSString *) serveNextCustomerInDeliLine:(NSMutableArray *)deliLine {
+    if (deliLine.count == 0) {
+        return nil;
+        }else{
+            NSString *firstName = deliLine[0];
+            [deliLine removeObject:firstName];
+            return firstName;
+            }
+    }
+
 
 @end
