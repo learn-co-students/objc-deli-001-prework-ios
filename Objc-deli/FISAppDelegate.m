ecccb2e9c4a547129084fa99e0633c2baf7116eb
William Robinson <wsrobins@uvm.edu>
2016-06-25 18:21:11 -0400

    Deli line methods // lab completed.

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -10,10 +10,42 @@
     return YES;
 }
 
-/*
- 
- *  Define your methods here!
- 
- */
+- (NSString *)stringWithDeliLine:(NSArray *)deliLine {
+    
+    NSString *customerQueue = @"";
+    
+    if ([deliLine count] > 0) {
+        
+        customerQueue = @"The line is:";
+        
+        for (NSInteger i = 0; i < [deliLine count]; i++) {
+            
+            customerQueue = [customerQueue stringByAppendingFormat:@"\n%li. %@", i + 1, deliLine[i]];
+        }
+    }
+    
+    else {
+        
+        customerQueue = @"The line is currently empty.";
+    }
+    
+    return customerQueue;
+}
+
+- addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine {
+    
+    [deliLine addObject:name];
+    
+    return nil;
+}
+
+- (NSString *)serveNextCustomerInDeliLine:(NSMutableArray *)deliLine {
+    
+    NSString *firstInLine = deliLine[0];
+    
+    [deliLine removeObjectAtIndex:0];
+    
+    return firstInLine;
+}
 
 @end
