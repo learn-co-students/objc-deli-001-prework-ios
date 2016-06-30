7c746193d5dc69034373949c3897d0ef8719adb1
OliviaGunton <olivia.gunton@gmail.com>
2016-06-30 14:50:06 -0400

    Implement print, add, and pop for deliLine array

--- Objc-deli/FISAppDelegate.h
+++ Objc-deli/FISAppDelegate.h
@@ -8,8 +8,19 @@
 
 /*
  
- *  Declare your methods here!
+ *  stringWithDeliLine: should accept an NSArray argument deliLine and return an NSString object.
+ addName:toDeliLine: should accept an NSString called name and an NSMutableArray called deliLine as arguments, and return nothing.
+ serveNextCustomerInDeliLine: should accept an NSMutableArray called deliLine as an argument and return an NSString.
  
  */
+
+//Return formatted string listing customers in deliLine, or message if line is empty.
+- (NSString *)stringWithDeliLine:(NSArray *)deliLine;
+
+//Add customer name to deliLine.
+- (void)addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine;
+
+//Pop first customer off of deliLine and return customer name as string.
+- (NSString *)serveNextCustomerInDeliLine: (NSMutableArray *)deliLine;
 
 @end
