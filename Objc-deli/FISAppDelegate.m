ded6e9ede09d7e2a483173cdb90c7509a8676152
Louis <Lharris134@gmail.com>
2016-07-27 00:02:07 -0400

    Couldn't get lab to work

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -10,10 +10,36 @@
     return YES;
 }
 
-/*
- 
- *  Define your methods here!
- 
- */
+-(NSString*)stringWithDeliLine:(NSArray*) deliLine
+{
+    NSString *empty = @"The line is currently empty.";
+    NSMutableString *namesInLine = [[NSMutableString alloc]init];
+    
+    if(self.deliLine == nil || [self.deliLine count] == 0)
+    {
+        return empty;
+    }
+    else
+    {
+        for(NSString *string in self.deliLine)
+        {
+            [namesInLine appendString:string];
+        }
+    }
+    return [NSString stringWithString:namesInLine];
+}
+
+-(void)addName:toDeliLine:(NSString*)name:(NSMutableArray*)deliLine
+{
+    [self.deliLine addObject:name];
+}
+
+-(NSString*)serveNextCustomerInDeliLine:(NSMutableArray*)deliLine
+{
+    NSString *servedCustomer = [self.deliLine objectAtIndex:0];
+    [self.deliLine removeObjectAtIndex:0];
+    
+    return servedCustomer;
+}
 
 @end
