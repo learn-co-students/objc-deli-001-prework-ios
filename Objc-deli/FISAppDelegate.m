e2cb7b1cd58c284d1b8c48c66565b4d9ebc7d517
aacohen <arielaades@gmail.com>
2016-08-15 15:11:29 -0400

    Done.

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -10,6 +10,30 @@
     return YES;
 }
 
+- (NSString *) stringWithDeliLine: (NSArray *) deliLine
+{
+    NSString *theLineIs = @"The line is:";
+    NSMutableString *deliLineB = [theLineIs mutableCopy];
+    
+    if ([deliLine count] ==0)
+    {NSString *emptyLine = @"The line is currently empty.";
+        return emptyLine;}
+    else {for (NSUInteger i=0; i< [deliLine count]; i++)
+    {NSInteger queueNumber = i+1;
+              NSMutableString *deliLineC = [NSMutableString stringWithFormat:  @"\n%li. %@", queueNumber, deliLine[i]];
+              [deliLineB appendString: deliLineC];
+        
+          }
+        return deliLineB;}
+}
+- (void) addName:(NSString *) name toDeliLine: (NSMutableArray *)deliLine
+{[deliLine addObject:name];
+}
+- (NSString *) serveNextCustomerInDeliLine: (NSMutableArray *) deliLine
+{NSString *firstName = deliLine[0];
+    [deliLine removeObjectAtIndex:0];
+    return firstName;
+}
 /*
  
  *  Define your methods here!
