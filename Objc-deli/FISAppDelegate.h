5d7f347fcb8bb56601e4d09fe162986f62bfd88e
Matt Tripodi <mtripodi1027@gmail.com>
2016-08-30 22:14:23 -0400

    Deli Counter Project

--- Objc-deli/FISAppDelegate.h
+++ Objc-deli/FISAppDelegate.h
@@ -12,4 +12,9 @@
  
  */
 
+- (NSString *)stringWithDeliLine:(NSMutableArray *)deliLine;
+- (NSMutableArray *)addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine;
+- (NSString *)serveNextCustomerInDeliLine:(NSMutableArray *)deliLine;
+
+
 @end
--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -16,4 +16,32 @@
  
  */
 
+- (NSString *)stringWithDeliLine:(NSMutableArray *)deliLine {
+    if ([deliLine count] == 0) {
+        
+        return @"The line is currently empty.";
+    }
+    
+    NSMutableString *mutableDeliLine = [NSMutableString string];
+    [mutableDeliLine appendString:@"The line is:"];
+    for (NSInteger i = 0; i < [deliLine count]; i++) {
+        NSString *newCustomer = [NSString stringWithFormat:@"\n%lu. %@", (i + 1), deliLine[i]];
+        [mutableDeliLine appendString:(NSString *)newCustomer];
+    }
+    return mutableDeliLine;
+}
+
+- (NSMutableArray *)addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine {
+    [deliLine addObject:name];
+    
+    return deliLine;
+}
+
+- (NSString *)serveNextCustomerInDeliLine:(NSMutableArray *)deliLine {
+    NSString *theFirstCustomer = deliLine[0];
+    [deliLine removeObjectAtIndex:0];
+    
+    return theFirstCustomer;
+}
+
 @end
