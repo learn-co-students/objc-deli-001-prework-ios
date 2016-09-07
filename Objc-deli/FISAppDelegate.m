9d63712caf9a7dbde85e47a7b605ec5780ce2c1b
Crystal Mora <cmora427@gmail.com>
2016-09-06 20:51:26 -0400

    done

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -10,6 +10,36 @@
     return YES;
 }
 
+-(NSString *)stringWithDeliLine:(NSArray*)deliLine {
+    if ([deliLine count] == 0) {
+        NSString *emptyLine = @"The line is currently empty.";
+        return emptyLine;
+    } else {
+        NSMutableString *peopleOnLine = [[NSMutableString alloc] init];
+        [peopleOnLine appendString: @"The line is:"];
+
+        for (int counter = 0; counter <deliLine.count; counter++) {
+            NSString *line = [NSString stringWithFormat:@"\n%i. %@", counter+1, deliLine[counter]];
+            [peopleOnLine appendString: line];
+        }
+        return peopleOnLine;
+    }
+    
+}
+
+-(void)addName:(NSString*)name toDeliLine:(NSMutableArray*)deliLine {
+    [deliLine addObject:name];
+}
+
+-(NSString *)serveNextCustomerInDeliLine:(NSMutableArray*)deliLine {
+    NSString *nextPersonOnLine = deliLine[0];
+    [deliLine removeObjectAtIndex:0];
+    return nextPersonOnLine;
+}
+
+                       
+                       
+
 /*
  
  *  Define your methods here!
